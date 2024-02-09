7c7db1972b7259263db1f55fc062884278e91379
ajujacob <ajujacob88@gmail.com>
2024-02-09 23:16:35 +0530

    quiz questions class model created,, also questions screen design created(alternate method - by using SizedBox)

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,9 +12,10 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return Center(
+    return SizedBox(
+      width: double.infinity,
       child: Column(
-        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           const Text('The question..'),
           const SizedBox(height: 30),
