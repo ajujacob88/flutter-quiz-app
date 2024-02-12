4cdb5ff34581736466eba502892782ac579e23a4
ajujacob <ajujacob88@gmail.com>
2024-02-12 13:04:13 +0530

    make Answer button custom widget

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,29 +13,22 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return SizedBox(
+    return const SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text('The question..'),
-          const SizedBox(height: 30),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Answer 1'),
+          Text(
+            'The question..',
+            style: TextStyle(
+              color: Colors.white,
+            ),
           ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Answer 2'),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Answer 3'),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Answer 4'),
-          ),
+          SizedBox(height: 30),
+          AnswerButton('Answer 1'),
+          AnswerButton('Answer 2'),
+          AnswerButton('Answer 3'),
+          AnswerButton('Answer 4'),
         ],
       ),
     );
