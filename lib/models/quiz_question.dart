fa37a806ba3456f51efe37a2582128a5e2c12cc6
ajujacob <ajujacob88@gmail.com>
2024-02-13 11:32:27 +0530

    shuffled lists of answers- shuffling the answers

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -4,8 +4,12 @@ class QuizQuestion {
   final String text;
   final List<String> answers;
 
+  //for shuffling the answers without changing the original lists... shuffle() will change the original list, so creating a copy and then shuffling
   List<String> getShuffledAnswers() {
-    final shuffledList = List.of(answers);
+    final shuffledList = List.of(answers); //copying the amswers to a new list
+    // print("printing1, $shuffledList");
+    // final ch = answers; //this wont create a copy,, this will create only a reference of the same list,, to create a copt of the list use list.of
+    // print("print22,$ch");
     shuffledList.shuffle();
     return shuffledList;
   }
