e5964db006f6fc84e32d4fcf8e1e89ce926a408a
ajujacob <ajujacob88@gmail.com>
2024-02-23 21:17:39 +0530

    combining columns and rows

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenAnswers});
@@ -12,7 +13,7 @@ class ResultsScreen extends StatelessWidget {
       summary.add(
         {
           'question_index': i,
-          'question': questions[i],
+          'question': questions[i].text,
           'correct_answer': questions[i].answers[0],
           //the answer is given in my question file is in such a way that the first option is the correct answer
           'user_answer': chosenAnswers[i],
@@ -33,7 +34,7 @@ class ResultsScreen extends StatelessWidget {
           children: [
             const Text('You answered X out of Y questions correctly'),
             const SizedBox(height: 30),
-            const Text('List of answers and questions'),
+            QuestionsSummary(getSummaryData()),
             const SizedBox(height: 30),
             TextButton(
               onPressed: () {},
